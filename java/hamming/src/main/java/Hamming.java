a0fb8434018541665daae7291af9b49637b45f4e
studio3104 <studio3104.com@gmail.com>
2021-01-09 05:06:08 +0900

    Answer "Hamming"

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,9 +1,31 @@
 public class Hamming {
+    private final String leftStrand;
+    private final String rightStrand;
+
     public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.isEmpty()) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+        if (rightStrand.isEmpty()) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
     }
 
     public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int d = 0;
+
+        for (int i = 0; i < leftStrand.length(); ++i) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
+                ++d;
+            }
+        }
+
+        return d;
     }
 }
