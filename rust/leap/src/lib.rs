1704aa3328db0a2d3d2ebdda4394470507eb69b3
studio3104 <studio3104.com@gmail.com>
2021-01-08 19:05:36 +0900

    Answer "Leap"

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    let mut is_leap = false;
+
+    if year % 4 == 0 {
+        if year % 100 != 0 {
+            is_leap = true;
+        } else if year % 400 == 0 {
+            is_leap = true;
+        }
+    }
+
+    is_leap
 }
