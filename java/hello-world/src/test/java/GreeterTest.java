5f986c4d276b8441cbf02584657a950c332ee6a6
studio3104 <studio3104.com@gmail.com>
2021-01-09 03:52:30 +0900

    Answer "Hello World"

--- java/hello-world/src/test/java/GreeterTest.java
+++ java/hello-world/src/test/java/GreeterTest.java
@@ -3,10 +3,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class GreeterTest {
-
     @Test
     public void testThatGreeterReturnsTheCorrectGreeting() {
         assertEquals("Hello, World!", new Greeter().getGreeting());
     }
-
 }
