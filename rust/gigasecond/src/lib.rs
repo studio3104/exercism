235e583753ed58cc315d9c2064e4cf63e5236008
studio3104 <studio3104.com@gmail.com>
2021-01-08 17:07:59 +0900

    Update "Gigasecond" according to the mentor's review comment

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,6 @@
 use chrono::{DateTime, Utc, Duration};
-use std::ops::Add;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start.add(Duration::seconds(1_000_000_000))
+    start + Duration::seconds(1_000_000_000)
 }
