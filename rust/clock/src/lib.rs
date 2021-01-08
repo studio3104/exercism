adae658a74f7b7ee56262efa386a5ce58a992f63
studio3104 <studio3104.com@gmail.com>
2021-01-09 02:12:21 +0900

    Answer "Clock"

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,26 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    pub hours: i32,
+    pub minutes: i32,
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
+    }
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Self {
+            hours: (hours + minutes.div_euclid(60)).rem_euclid(24),
+            minutes: minutes.rem_euclid(60),
+        }
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+    pub fn add_minutes(self, minutes: i32) -> Self {
+        Self::new(self.hours, self.minutes + minutes)
     }
 }
